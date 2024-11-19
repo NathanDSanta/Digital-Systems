f289b6b7adb4c1ac3ed7546ee83e53f87d6d9be0
Nivis-star <128679700+Nivis-star@users.noreply.github.com>
2024-11-19 23:18:22 +0100

    Small changes hope it works now

--- practiques/alu/alu.vhdl
+++ practiques/alu/alu.vhdl
@@ -1,50 +1,51 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
- 
+
 entity alu is
- Port ( inp_a : in STD_LOGIC_VECTOR(4 downto 0);
- inp_b : in STD_LOGIC_VECTOR (4 downto 0);
- sel : in STD_LOGIC_VECTOR (3 downto 0);
- display1 : out STD_LOGIC_VECTOR(7 downto 0);
- display2 : out STD_LOGIC_VECTOR(7 downto 0);
- display3 : out STD_LOGIC_VECTOR(7 downto 0);
- out_alu : out STD_LOGIC_VECTOR (4 downto 0));
+    Port ( inp_a : in STD_LOGIC_VECTOR(4 downto 0);
+           inp_b : in STD_LOGIC_VECTOR (4 downto 0);
+           sel : in STD_LOGIC_VECTOR (3 downto 0);
+           display1 : out STD_LOGIC_VECTOR(7 downto 0);
+           display2 : out STD_LOGIC_VECTOR(7 downto 0);
+           display3 : out STD_LOGIC_VECTOR(7 downto 0);
+           out_alu : out STD_LOGIC_VECTOR (4 downto 0));
 end alu;
- 
+
 architecture behaviour of alu is
-  signal A,B: STD_LOGIC_VECTOR (4 downto 0);
+    signal A, B, resultat : STD_LOGIC_VECTOR (4 downto 0);
 begin
-  process(inp_a,inp_b, sel)
-  begin
-    A <= inp_a;
-    B <= inp_b;
-    case sel is
-    when "0001" => -- Comparacio
-      out_alu <= "00000";
-      if(A < B) then
-        display1 <= "00000000";
-        display2 <= STD_LOGIC_VECTOR(to_unsigned(80,8));
-        display3 <= STD_LOGIC_VECTOR(to_unsigned(111,8));
-      elsif (A = B) then
-        display1 <= "00000000";
-        display2 <= STD_LOGIC_VECTOR(to_unsigned(121,8));
-        display3 <= STD_LOGIC_VECTOR(to_unsigned(103,8));
-      else
-        display1 <= "00000000";
-        display2 <= STD_LOGIC_VECTOR(to_unsigned(56,8));
-        display3 <= STD_LOGIC_VECTOR(to_unsigned(111,8));
-      end if;
-    when "0010" => -- Suma
-      out_alu <= STD_LOGIC_VECTOR(unsigned(A) + unsigned(B));
-    when "0100" => -- Resta
-      out_alu <= STD_LOGIC_VECTOR(unsigned(A) - unsigned(B));
-    when "1000" => -- Shift
-      out_alu <= STD_LOGIC_VECTOR(shift_left(unsigned(inp_a), to_integer(unsigned(inp_b))));
-    when others =>
-      display1 <= STD_LOGIC_VECTOR(to_unsigned(121,8));
-      display2 <= STD_LOGIC_VECTOR(to_unsigned(80,8));
-      display3 <= STD_LOGIC_VECTOR(to_unsigned(80,8));
-    end case;
-  end process ;
+    process(inp_a, inp_b, sel)
+    begin
+        A <= inp_a;
+        B <= inp_b;
+        case sel is
+            when "0001" => -- Comparacio
+                if (A < B) then
+                    display1 <= "00000000";
+                    display2 <= STD_LOGIC_VECTOR(to_unsigned(80, 8));
+                    display3 <= STD_LOGIC_VECTOR(to_unsigned(111, 8));
+                elsif (A = B) then
+                    display1 <= "00000000";
+                    display2 <= STD_LOGIC_VECTOR(to_unsigned(121, 8));
+                    display3 <= STD_LOGIC_VECTOR(to_unsigned(103, 8));
+                else
+                    display1 <= "00000000";
+                    display2 <= STD_LOGIC_VECTOR(to_unsigned(56, 8));
+                    display3 <= STD_LOGIC_VECTOR(to_unsigned(111, 8));
+                end if;
+            when "0010" => -- Suma
+                resultat <= STD_LOGIC_VECTOR(unsigned(A) + unsigned(B));
+            when "0100" => -- Resta
+                resultat <= STD_LOGIC_VECTOR(unsigned(A) - unsigned(B));
+            when "1000" => -- Shift
+                resultat <= STD_LOGIC_VECTOR(shift_left(unsigned(inp_a), to_integer(unsigned(inp_b))));
+            when others =>
+                display1 <= STD_LOGIC_VECTOR(to_unsigned(121, 8));
+                display2 <= STD_LOGIC_VECTOR(to_unsigned(80, 8));
+                display3 <= STD_LOGIC_VECTOR(to_unsigned(80, 8));
+        end case;
+        
+        out_alu <= resultat;
+    end process;
 end behaviour;
--- practiques/alu/alu_tb.vhdl
+++ practiques/alu/alu_tb.vhdl
@@ -6,30 +6,40 @@ entity alu_tb is
 end entity alu_tb;
 
 architecture rtl of alu_tb is
-  signal A,B,F : std_logic_vector(4 downto 0);
+  signal A, B, F : std_logic_vector(4 downto 0);
   signal sel : std_logic_vector(3 downto 0);
   signal display1 : std_logic_vector(7 downto 0);
   signal display2 : std_logic_vector(7 downto 0);
   signal display3 : std_logic_vector(7 downto 0);
 begin
   G1 : entity work.alu(behaviour) port map(
-                inp_a => A, inp_b => B,
-                sel => sel, display1 => display1,
-                display2 => display2, display3 => display3,
-                out_alu => F
-              ); 
+          inp_a => A, inp_b => B,
+          sel => sel, display1 => display1,
+          display2 => display2, display3 => display3,
+          out_alu => F
+        ); 
+
   process is 
   begin
+    -- Cas de test 1: Comparació (A < B)
     sel <= "0001";
-    A <= "01100";
-    B <= "00010";
+    A <= "01100"; -- A = 12
+    B <= "00010"; -- B = 2
     wait for 10 ns;
+
+    -- Cas de test 2: Suma (A + B)
     sel <= "0010";
     wait for 10 ns;
+
+    -- Cas de test 3: Resta (A - B)
     sel <= "0100";
     wait for 10 ns;
+
+    -- Cas de test 4: Shift (A << B)
     sel <= "1000";
     wait;
+
+    -- Fi de la simulació
   end process;
   
 end architecture rtl;
