eaa01a093c9207202c5e4bda35f44eaa94c1ff22
NathanDSanta <natan.d.santa@gmail.com>
2024-11-20 11:41:13 +0100

    Assert i report afegits

--- practiques/alu/alu_tb.vhdl
+++ practiques/alu/alu_tb.vhdl
@@ -25,18 +25,25 @@ begin
     sel <= "0001";
     A <= "01100"; -- A = 12
     B <= "00010"; -- B = 2
+    assert display1 = "00000000"report "Display 1 correcta" severity note;
+    assert display2 = STD_LOGIC_VECTOR(to_unsigned(80, 8)) report "Display 2 correcta" severity note;
+    assert display3 = STD_LOGIC_VECTOR(to_unsigned(111, 8)) report "Display 3 correcta" severity note;
     wait for 10 ns;
 
     -- Cas de test 2: Suma (A + B)
     sel <= "0010";
+    assert F="01110" report "Suma correcta" severity note;
     wait for 10 ns;
 
     -- Cas de test 3: Resta (A - B)
     sel <= "0100";
+    assert F="01010" report "Resta correcta" severity note;
     wait for 10 ns;
 
     -- Cas de test 4: Shift (A << B)
     sel <= "1000";
+    assert F="10000" report "Shift correcte" severity note;
+    wait for 10 ns;
     wait;
 
     -- Fi de la simulació
